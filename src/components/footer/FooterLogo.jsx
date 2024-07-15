ac3bd9a933ef9010157e550c5ec9b925bae11147
IQ-Hck <130089245+Jeevons@users.noreply.github.com>
2024-07-15 17:19:55 +0200

    fixing litle bugs in css of footer and site map

--- src/components/footer/FooterLogo.jsx
+++ src/components/footer/FooterLogo.jsx
@@ -3,12 +3,14 @@ import "./Footer.css";
 
 const FooterLogo = () => {
   const basePath = import.meta.env.BASE_URL;
-  
-  <div className="footer-logo">
-    <Link to="/" className="logo hover-target">
-      <img src={`${basePath}assets/logos/jeevonsLogo.png`} alt="Jeevons Logo" />
-    </Link>
-  </div>;
+
+  return (
+    <div className="footer-logo">
+      <Link to="/" className="logo hover-target">
+        <img src={`${basePath}assets/logos/jeevonsLogo.png`} alt="Jeevons Logo" />
+      </Link>
+    </div>
+  );
 };
 
 export default FooterLogo;
